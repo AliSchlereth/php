337abb196d182410686a2de543161e31247ba0b3
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-20 21:03:08 -0700

    Refactor Hamming

--- hamming/hamming.php
+++ hamming/hamming.php
@@ -1,15 +1,13 @@
 <?php
 
-//
-// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 function distance($a, $b)
 {
     if (strlen($a)!=strlen($b)){
         throw new InvalidArgumentException('DNA strands must be of equal length.');
+    } else if ($a == $b) {
+        return 0;
     }
+
     $hamming = 0;
     for($i = 0; $i < strlen($a); $i++) {
         if ($a[$i] != $b[$i]) {
