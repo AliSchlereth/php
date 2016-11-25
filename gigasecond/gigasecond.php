f144b2496b463a4158ff6d4ede29306c60fcb40c
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-25 12:53:46 -0700

    Non mutation version

--- gigasecond/gigasecond.php
+++ gigasecond/gigasecond.php
@@ -3,7 +3,8 @@
     function from($date)
     {
         $gs = (string)10**9;
-        return $date->add(new DateInterval('PT'.$gs.'S'));
+        $new_date = (clone $date);
+        return $new_date->add(new DateInterval('PT'.$gs.'S'));
     }
 
  ?>
--- gigasecond/gigasecond_test.php
+++ gigasecond/gigasecond_test.php
@@ -38,7 +38,6 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
 
     public function testDateWithTime1()
     {
-        $this->markTestSkipped();
         $date = GigasecondTest::dateSetup("2015-01-24 22:00:00");
         $gs = from($date);
 
@@ -47,7 +46,6 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
 
     public function testDateWithTime2()
     {
-        $this->markTestSkipped();
         $date = GigasecondTest::dateSetup("2015-01-24 23:59:59");
         $gs = from($date);
 
@@ -56,7 +54,6 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
 
     public function testNoMutation()
     {
-        $this->markTestSkipped();
         $date = GigasecondTest::dateSetup("2015-01-24");
         $gs = from($date);
 
